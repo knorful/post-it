2f3f15da90e3c721f937cbfb259a1deb0751ca0b
Kris Norful <krisnorful@gmail.com>
2021-03-04 18:58:55 -0600

    show/hide comments for individual posts

---
+++ src/components/Posts/Post/Comments/CommentReplies/CommentReplies.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { format } from "timeago.js";
+
+import classes from "./CommentReplies.module.css";
+
+class CommentReplies extends Component {
+  state = {
+    showReplies: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      showReplies: !this.state.showReplies,
+    });
+  };
+  formatTime = (time) => {
+    return format(time * 1000);
+  };
+  render() {
+    let showReplies = this.props.replies.data.children.map((reply) =>
+      reply.data.hasOwnProperty("created_utc") ? (
+        <div className={classes.Content}>
+          <div className={classes.Tagline}>
+            <p className={classes.Author}>{reply.data.author}</p>
+            <div className={classes.Timestamp}>
+              <p>{this.formatTime(reply.data.created_utc)}</p>
+            </div>
+          </div>
+          <div className={classes.Body}>
+            <p>{reply.data.body}</p>
+          </div>
+        </div>
+      ) : null
+    );
+    return (
+      <div className={classes.Replies}>
+        <button className={classes.Btn} onClick={this.handleClick}>
+          {!this.state.showReplies ? (
+            <p>{showReplies.length - 1} Replies</p>
+          ) : (
+            <p>Close Replies</p>
+          )}
+        </button>
+        {this.state.showReplies ? showReplies : null}
+      </div>
+    );
+  }
+}
+
+export default CommentReplies;
--- src/components/Posts/Post/Comments/Comments.js
+++ src/components/Posts/Post/Comments/Comments.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
+import { format } from "timeago.js";
+import CommentReplies from "./CommentReplies/CommentReplies";
 
 import classes from "./Comments.module.css";
 
 class Comments extends Component {
+  state = {
+    showComments: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      showComments: !this.state.showComments,
+    });
+  };
+
+  formatTime = (time) => {
+    return format(time * 1000);
+  };
+
   render() {
+    let comments = this.props.comments.map((comment) => (
+      <div className={classes.Container}>
+        <div className={classes.Tagline}>
+          <p className={classes.Author}>{comment.data.author}</p>
+          <div className={classes.Timestamp}>
+            <p>{this.formatTime(comment.data.created_utc)}</p>
+          </div>
+        </div>
+
+        <div className={classes.Content}>
+          <p className={classes.Body}>{comment.data.body}</p>
+          <div>
+            {comment.data.replies ? (
+              <CommentReplies replies={comment.data.replies} />
+            ) : null}
+          </div>
+        </div>
+      </div>
+    ));
     return (
       <div className={classes.Comments}>
-        <button className={classes.Btn}>
+        <button onClick={this.handleClick} className={classes.Btn}>
           <i className="far fa-comment"></i>
-          <p className={classes.FooterText}>{this.props.numComments}</p>
+          <p className={classes.FooterText}>{this.props.numComments} </p>
+          {this.state.showComments ? (
+            <p className={classes.Close_Open}>Close Comments</p>
+          ) : (
+            <p className={classes.Close_Open}> Comments</p>
+          )}
         </button>
+        {this.state.showComments ? comments : null}
       </div>
     );
   }
